1556bdccffc57117f6a77c492874951afe257628
Farhan Ishraq <ishraqf@student.ubc.ca>
2021-05-11 01:51:22 +0600

    Added wireless communication with mobile app

--- ArduinoCar/ArduinoCar.ino
+++ ArduinoCar/ArduinoCar.ino
@@ -3,6 +3,9 @@
  * Purpose: The program takes input from the joystick module to set the speed and direction of the gear motors
  */
 
+#include <SoftwareSerial.h>
+SoftwareSerial BT(2, 3);
+
 /* enable pins for right and left motors */
 #define ENA 9
 #define ENB 10
@@ -13,24 +16,20 @@
 #define IN3 6
 #define IN4 7
 
-/* analog pins to receive the x and y values from joystick*/
-const int VRyPin = A1;
-const int VRxPin = A0;
-
 /* stores x and y values from the joystick */
-int xAxis = 0;
-int yAxis = 0;
+int xAxis = 140;
+int yAxis = 140;
 
 /* set the speed of the right and left motors*/ 
 int mSpeedRight = 0;
 int mSpeedLeft = 0;
 
-/* maps the y values from 0-1023 to 0-255*/
-int yMapped = 0;
+/* maps the x values from 0-1023 to 0-255*/
+int xMapped = 0;
 
 /* function prototypes */
-void readXaxis(int);
 void readYaxis(int);
+void readXaxis(int);
 
 void setup() {
   /* set all pins as OUTPUT*/
@@ -42,24 +41,28 @@ void setup() {
   pinMode(IN4, OUTPUT);
 
   /* defaul communication frequency */
-  Serial.begin(9600);
+  BT.begin(9600);
 }
 
 void loop() {
-  /* read the x and y axis values */
-  xAxis = analogRead(VRxPin);
-  yAxis = analogRead(VRyPin);
-
+  /* Read the x and y values from mobile app */
+  while (BT.available() >= 2) {
+    xAxis = BT.read();
+    delay(10);
+    yAxis = BT.read();
+  }
+  delay(10);
+  
   /* use the x and y values to set the speed and direction of the motors*/
-  readXaxis(xAxis);
   readYaxis(yAxis);
+  readXaxis(xAxis);
 
   /* provent the motors from buzzing due to low speed */
-  if (mSpeedLeft < 80) {
+  if (mSpeedLeft < 50) {
     mSpeedLeft = 0;
   }
 
-  if (mSpeedRight < 80) {
+  if (mSpeedRight < 50) {
     mSpeedRight = 0;
   }
 
@@ -70,24 +73,11 @@ void loop() {
 
 
 /*
- * Purpose: checks the x values from the joystick to determine the forward and backward movement of the car
- * Param: xAxis -- x values from the joystick
+ * Purpose: checks the y values from the joystick to determine the forward and backward movement of the car
+ * Param: yAxis -- y values from the joystick
  */
-void readXaxis(int xAxis) {
-  if (xAxis < 514) {
-    /* for right motor */
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-
-    /* for left motor */
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-
-    /* map the speeds */
-    mSpeedRight = map(xAxis, 514, 0, 0, 255);
-    mSpeedLeft = map(xAxis, 514, 0, 0, 255);
-  }
-  else if (xAxis > 520) {
+void readYaxis(int yAxis) {
+  if (yAxis > 160) { // move backward
     /* for right motor */
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
@@ -97,8 +87,21 @@ void readXaxis(int xAxis) {
     digitalWrite(IN4, HIGH);
 
     /* map the speeds */
-    mSpeedRight = map(xAxis, 520, 1023, 0, 255);
-    mSpeedLeft = map(xAxis, 520, 1023, 0, 255);
+    mSpeedRight = map(yAxis, 140, 220, 0, 255);
+    mSpeedLeft = map(yAxis, 140, 220, 0, 255);
+  }
+  else if (yAxis < 120) { // move forward
+    /* for right motor */
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+
+    /* for left motor */
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+
+    /* map the speeds */
+    mSpeedRight = map(yAxis, 140, 60, 0, 255);
+    mSpeedLeft = map(yAxis, 140, 60, 0, 255);
   }
   else {
     /* joystick is idle */
@@ -109,17 +112,17 @@ void readXaxis(int xAxis) {
 
 
 /*
- * Purpose: checks the y values from the joystick to determine the left and right movement of the car
- * Param: yAxis -- y values from the joystick
+ * Purpose: checks the x values from the joystick to determine the left and right movement of the car
+ * Param: xAxis -- x values from the joystick
  */
-void readYaxis(int yAxis) {
-  if (yAxis < 520) {
-    /* map the yAxis values */
-    yMapped = map(yAxis, 520, 0, 0, 255);
-
+void readXaxis(int xAxis) {
+  if (xAxis < 120) { // move left
+    /* map the xAxis values */
+    xMapped = map(xAxis, 140, 60, 0, 255);
+    //xMapped = xAxis;
     /* set the speeds */
-    mSpeedRight = mSpeedRight + yMapped;
-    mSpeedLeft = mSpeedLeft - yMapped;
+    mSpeedRight = mSpeedRight + xMapped;
+    mSpeedLeft = mSpeedLeft - xMapped;
 
     /* keep speed within threshold */
     if (mSpeedRight > 255) {
@@ -129,13 +132,13 @@ void readYaxis(int yAxis) {
       mSpeedLeft = 0;
     }
   }
-  else if (yAxis > 528) { 
-    /* map the yAxis values */
-    yMapped = map(yAxis, 528, 1023, 0, 255);
-
+  else if (xAxis > 160) { // move right
+    /* map the xAxis values */
+    xMapped = map(xAxis, 140, 220, 0, 255);
+    //xMapped = xAxis;
     /* set the speeds */
-    mSpeedLeft = mSpeedLeft + yMapped;
-    mSpeedRight = mSpeedRight - yMapped;
+    mSpeedLeft = mSpeedLeft + xMapped;
+    mSpeedRight = mSpeedRight - xMapped;
 
     /* keep within threshold */
     if (mSpeedLeft > 255) {
